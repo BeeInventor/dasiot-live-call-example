a66ac7889d4d161d2dcaef910cd18e1294e6eee5
Steven Chen <steven11329@gmail.com>
2024-01-30 11:08:18 +0800

    chore: add base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "dasiot-live-call-example",
   plugins: [react()],
   server: {
     host: true,
@@ -13,4 +14,4 @@ export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
-})
+});
